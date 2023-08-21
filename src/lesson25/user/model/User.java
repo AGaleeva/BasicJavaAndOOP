f52e15769081b841115a712051815fee3b40e202
AGaleea <98322303+AGaleea@users.noreply.github.com>
2023-08-21 17:31:18 +0200

    User was a bit changed

--- src/lesson25/user/model/User.java
+++ src/lesson25/user/model/User.java
@@ -21,6 +21,44 @@ public class User {
         }
     }
 
+    /*
+    TODO Homework
+    1) Min 8 symbols
+    2) Min 1 capital letter symbol
+    3) Min 1 small letter symbol
+    4) Min 1 digit
+    6) Min 1 special symbol (! % @ * &)
+    */
+
+    /*
+    1) '@' - exists and only one
+    2) '.' - min one dot after '@'
+    3) min two symbols after last dot
+    4) alphabetic, digits, '@', '.', '_', '-'
+    */
+    private boolean emailIsValid(String email) {
+        int at = email.indexOf('@');
+        if (at == -1 || (email.indexOf('@', at + 1) >= 0)) {
+            return false;
+        }
+//        if (email.lastIndexOf('@') != at) { //  это такой же метод, как и выше, но с методом lastIndexOf
+        // если один индекс не равен другому индексу:
+        // у нас минимум два символа '@', т.е. мэйл не валиден
+//            return false;
+//        }
+        if (email.indexOf('.', at + 1) == -1) return false;
+
+        if (email.lastIndexOf('.') >= email.length() - 2) return false;
+        for (int i = 0; i < email.length(); i++) {
+            char c = email.charAt(i);
+//            if (c >= '0' && c <= '9') return false; // проверяем, не цифра ли это по таблице ASCII
+            if (!(Character.isDigit(c) || Character.isAlphabetic(c) || c == '@' || c == '.' || c == '_' || c == '-'))
+                return false;
+        }
+
+        return true;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -59,44 +97,6 @@ public class User {
             if(uppCase && loweCase && digit && specSymbol) return true;
         }
         return false;
-    }
-
-    /*
-    TODO Homework
-    1) Min 8 symbols
-    2) Min 1 capital letter symbol
-    3) Min 1 small letter symbol
-    4) Min 1 digit
-    6) Min 1 special symbol (! % @ * &)
-    */
-
-    /*
-    1) '@' - exists and only one
-    2) '.' - min one dot after '@'
-    3) min two symbols after last dot
-    4) alphabetic, digits, '@', '.', '_', '-'
-    */
-    private boolean emailIsValid(String email) {
-        int at = email.indexOf('@');
-        if (at == -1 || (email.indexOf('@', at + 1) >= 0)) {
-            return false;
-        }
-//        if (email.lastIndexOf('@') != at) { //  это такой же метод, как и выше, но с методом lastIndexOf
-        // если один индекс не равен другому индексу:
-        // у нас минимум два символа '@', т.е. мэйл не валиден
-//            return false;
-//        }
-        if (email.indexOf('.', at + 1) == -1) return false;
-
-        if (email.lastIndexOf('.') >= email.length() - 2) return false;
-        for (int i = 0; i < email.length(); i++) {
-            char c = email.charAt(i);
-//            if (c >= '0' && c <= '9') return false; // проверяем, не цифра ли это по таблице ASCII
-            if (!(Character.isDigit(c) || Character.isAlphabetic(c) || c == '@' || c == '.' || c == '_' || c == '-'))
-                return false;
-        }
-
-        return true;
     }
 
     @Override
